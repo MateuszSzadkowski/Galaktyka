47b6d9b4f4da9447ab3cce6446aa23477f9a37ee
MateuszSzadkowski <szadkowskimateusz100@gmail.com>
2026-05-08 18:04:36 +0200

    Lab 4

--- IsUpdatable.h
+++ IsUpdatable.h
@@ -1,8 +1,6 @@
 #ifndef ISUPDATABLE_H
 #define ISUPDATABLE_H
 
-
-
 class IsUpdatable {
 public:
     bool isPaused{};
@@ -16,7 +14,5 @@ public:
     [[nodiscard]] virtual bool IsActive() const;
     [[nodiscard]] virtual int GetLastUpdate() const;
 };
-
-
 
 #endif //ISUPDATABLE_H
